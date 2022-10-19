bf14aa20a2f11f117a37c57b47e004e48459a5c0
Dhruv Thakkar <dhruvthakkar1768@gmail.com>
2022-10-18 22:57:32 -0400

    Final Version

--- src/com/project/Demo.java
+++ src/com/project/Demo.java
@@ -1,8 +1,5 @@
 package com.project;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
 public class Demo {
     public static void main(String[] args) {
         new SignInPage();
